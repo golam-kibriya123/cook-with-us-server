37eec3e5a5063f619ac9302e3ac017c7572655fe
golam-kibriya123 <golamkibriya622@gmail.com>
2023-05-02 00:37:55 -0500

    added chef data

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const chef = require('./data/chef.json')
 app.get('/', (req, res) => {
     res.send('cook-with-us is running')
 })
-
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const findByID = chef.find(chef => chef.id === id) || {};
+    res.send(findByID)
+})
 
 app.listen(port, () => {
     console.log(`cook-with-us API is running on port:  ${port}`)
