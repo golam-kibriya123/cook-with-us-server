a920e8e84bf9ec818853f68af5b1f0d9fa38f5df
golam-kibriya123 <golamkibriya622@gmail.com>
2023-05-05 01:17:44 -0500

    added single recipe data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const chef = require('./data/chef.json')
+const chef = require('./data/chef.json');
+const recipes = require('./data/recipe.json');
 
 app.get('/', (req, res) => {
     res.send('cook-with-us is running')
@@ -17,6 +18,11 @@ app.get('/chef/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const findByID = chef.find(chef => chef.id === id) || {};
     res.send(findByID)
+})
+app.get('/recipe/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const recipe = recipes.find(re => re.r_id == id);
+    res.send(recipe)
 })
 
 app.listen(port, () => {
